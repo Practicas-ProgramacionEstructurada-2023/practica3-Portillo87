5048ad1a34cb0e4b503d5f42877976830e13c46b
Portillo87 <wilfredovillalta828@gmail.com>
2023-08-26 20:46:45 -0600

    Practica3

--- U20230820_Practica3.cs
+++ U20230820_Practica3.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿﻿using System;
 
 namespace MyApp// Note: actual namespace depends on the project name.
 {
@@ -7,45 +6,46 @@ namespace MyApp// Note: actual namespace depends on the project name.
     {
          static void Main(string[] args)
         {
+            //Declaracion de variables
+            string? Marca_Zapato;
+            string? Color_Zapato;
+            int Cantidad;
+            double Precio_Zapato;
 
-        //Declaraciom de variables
-        string?  Marca_Zapato;
-        string? Color_Zapato;
-        int Cantidad;
-        double Precio_Zapatos;
+            //Capturando datos
+            Console.WriteLine("\n------- Capturando Variables -------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Ingrese la marca de zapatos a comprar:");
+            Marca_Zapato = Convert.ToString(Console.ReadLine());
 
-        //Capturando datos
-        Console.WriteLine("\n-------Capturando Variables--------\n");
-        Console.WriteLine("Ingrese la macar de zapatos a comprar:\n");
-        Marca_Zapato = Convert.ToString(Console.ReadLine ());
+            Console.WriteLine("\nIngrese el color del par de zapatos:");
+            Color_Zapato = Convert.ToString(Console.ReadLine());
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nIngrese la cantidad de pares de zapatos a comprar:");
+            Cantidad = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("\nIngrese el color del par de zapatos:\n");
-        Color_Zapato = Convert.ToString(Console.ReadLine ());
+            Console.WriteLine("\nIngrese el precio del par de zapatos a comprar:");
+            Precio_Zapato = Convert.ToDouble(Console.ReadLine());
+            Console.ResetColor();
+            Console.WriteLine("------- Fin de capturacion de variables -------\n");
 
-        Console.WriteLine("\nIngrese la cantidad de pares de zapatos a comprar:\n");
-        Cantidad = Convert.ToInt32(Console.ReadLine ());
+            //Capturando el total a pagar
+            double Total = Cantidad * Precio_Zapato;
 
-        Console.WriteLine("\nIngrese el precio del par de zapatos a comprar: \n");
-        Precio_Zapatos = Convert.ToDouble(Console.ReadLine ());
-        Console.WriteLine("------Fin de capturas------\n");
-
-        //Capturando el total a pagar
-        double Total = Cantidad * Precio_Zapatos;
-
-        //Imprimiendo resultado
-        Console.WriteLine("------Imprimiendo Resultados ------");
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("\nLa marca de zapatos a comprar es: " + Marca_Zapato);
-        Console.WriteLine("\nEl color de zapatos es: " + Color_Zapato);
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("\nLa cantiadad de pares de zapatos a comprar es: " + Cantidad);
-        Console.WriteLine("\nEl precio de zapatos a comprar es: " + Precio_Zapatos);
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("\nEl total a pagar es: " + Total);
-        Console.WriteLine("------Fin de imprimiendo Resultados ------\n");
-        
-
-
+            //Imprimiendo resultando
+            Console.WriteLine("\n------- Imprimiendo resultados -------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("La marca de zapatos a comprar es: " + Marca_Zapato);
+            Console.WriteLine("\nEl color de zapatos a comprar es: " + Color_Zapato);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nLa cantidad de pares de zapatos a comprar es: " + Cantidad);
+            Console.WriteLine("\nEl precio por par de zapatos a comprar es: " + Precio_Zapato);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nEL total a pagar es: " + Total);
+            Console.ResetColor();
+            Console.WriteLine("------- Fin de imprimir resultados -------\n");
         }
     }
 }
